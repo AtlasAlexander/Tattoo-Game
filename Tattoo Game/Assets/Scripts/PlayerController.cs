b9bb31833a45fe9e2e2989ab2a8e98d24c72e11a
Atlas <alexander2.al-muaini@live.uwe.ac.uk>
2026-04-15 08:42:58 +0100

    Player Controller + Basic Camera

---
+++ Tattoo Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float groundDist;
+
+    public LayerMask floor;
+    public Rigidbody rb;
+    public SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FloorCheck();
+        HandleMovement();
+    }
+    private void HandleMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 moveDir = new Vector3(x, 0, y);
+        rb.velocity = moveDir * speed;
+
+        if (x != 0 && x < 0)
+        {
+            sr.flipX = true;
+        }
+
+        if (x != 0 && x > 0)
+        {
+            sr.flipX = false;
+        }
+    }
+
+    private void FloorCheck()
+    {
+        RaycastHit hit;
+        Vector3 castPos = transform.position;
+        castPos.y += 1;
+        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, floor))
+        {
+            if (hit.collider != null)
+            {
+                Vector3 movePos = transform.position;
+                movePos.y = hit.point.y + groundDist;
+                transform.position = movePos;
+            }
+        }
+    }
+}
