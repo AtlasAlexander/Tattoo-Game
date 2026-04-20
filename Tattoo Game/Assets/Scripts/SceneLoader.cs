b93d6e12b8f0fa7cd420fcfa460f984b81654f28
Atlas <alexander2.al-muaini@live.uwe.ac.uk>
2026-04-20 17:26:42 +0100

    Bedroom Grey Box
    + Basic Scene Transition Script
    + Grey Box bedroom scene
    + Alternate NPC Added
    
    Next steps:
    Add do not unload game manager for data transfer
    Add 2nd main scene run
        including, new spawn position, flip player asset, hide npc 1, show npc 2, new npc interaction, tattoo parlour, end screens
    
    Add Interactivity to bedroom scene
     Food, Numbing Cream, Sleep

--- Tattoo Game/Assets/Scripts/SceneLoader.cs
+++ Tattoo Game/Assets/Scripts/SceneLoader.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public int sceneInt;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneInt);
+        }
+    }
     // Switch using scene name
     public void LoadScene(string sceneName)
     {
