b60b8cee4071f3ce4d1211e12e9ed0187b1db3f4
Atlas <alexander2.al-muaini@live.uwe.ac.uk>
2026-04-22 10:39:47 +0100

    Basic NPC Manager
    hides/reveals appropiate NPC
    
    Next to add:
    End Screen Manager

---
+++ Tattoo Game/Assets/Scripts/Managers/NPCManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour
+{
+  
+    public GameObject[] NPCs;
+
+    // Collates all objects with tag NPC into a list for later usage
+    public void FindNPCs() 
+    {
+        NPCs = GameObject.FindGameObjectsWithTag("NPC");
+    }
+
+    // This function makes all NPC's that are not the one needed invisable.
+    // This works fine for the purpose of this game but will need to be heavily built upon if you need more than 1 npc present per screen
+    public void MakeVisable(string NPCName)
+    {
+        foreach (GameObject npc in NPCs)
+        {
+            if (npc.name == NPCName)
+            {
+                npc.SetActive(true);
+            }
+            else if (npc.name != NPCName)
+            {
+                npc.SetActive(false);
+            }
+        }
+    }
+}
